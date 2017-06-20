9a044cb63c85d219387b2e7747e340c29a2ccb82
Chaitanya <chaitanya.kulkarni2334@gmail.com>
2017-06-20 15:25:35 -0400

    now prints current file count vs total no of files in the folder to be processed.

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ from tiler import WholeSlideTiler
 
 def main():
     # open input_path, and process each wholeslide image
-    for slidepath in glob.glob(cfg.IMAGE_FOLDER_PATH + '*.svs'):
+    files = glob.glob(cfg.IMAGE_FOLDER_PATH + '*.svs')
+    i = 1
+    for slidepath in files:
+        print("processing {0} of {1} whole slide images".format(i, len(files)))
         print(slidepath)
         basename = os.path.splitext(os.path.basename(slidepath))[0]
         basepath = os.path.join(cfg.OUTPUT_FOLDER_PATH, basename)
         WholeSlideTiler(slidepath, basepath, cfg.IMG_FORMAT, cfg.TILE_SIZE, cfg.OVERLAP, cfg.LIMIT_BOUNDS, cfg.QUALITY,
                         cfg.NUM_WORKERS, cfg.ONLY_LAST).run()
+        i+=1
 
 
 if __name__ == '__main__':
